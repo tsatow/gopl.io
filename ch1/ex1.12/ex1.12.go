d65429f64327ad0100e7a33a3225cc584158342e
tsatow <ta-satoh@atware.co.jp>
2020-04-24 13:57:35 +0900

    ccc

--- ch1/ex1.12/ex1.12.go
+++ ch1/ex1.12/ex1.12.go
@@ -49,7 +49,7 @@ func lissajous(out io.Writer, cycles int) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
